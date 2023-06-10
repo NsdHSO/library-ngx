9f83fb8a175462775173008e147e0f828e364504
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-06-10 23:34:31 +0300

    chore:🌻 added bottom line style

--- projects/ngx-ds/src/lib/components/carousel/arrow-carousel/arrow-carousel.component.ts
+++ projects/ngx-ds/src/lib/components/carousel/arrow-carousel/arrow-carousel.component.ts
@@ -2,7 +2,9 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxBtxDirective } from '../../../ngx-btn.directive';
 import { MatIconModule } from '@angular/material/icon';
-
+/**
+ * Represents a arrow carousel component.
+ */
 @Component({
   selector: 'lib-arrow-carousel',
   standalone: true,
@@ -11,6 +13,21 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrls: ['./arrow-carousel.component.scss'],
 })
 export class ArrowCarouselComponent {
+  /**
+   * Indicates whether the left arrow button is disabled.
+   * @type {boolean}
+   */
   @Input() arrowLeftDisabled = false;
+
+  /**
+   * Indicates whether the right arrow button is disabled.
+   * @type {boolean}
+   */
   @Input() arrowRightDisabled = false;
+
+  /**
+   * Indicates whether the arrows are disabled.
+   * @type {boolean}
+   */
+  @Input() disabledArrows = false;
 }
